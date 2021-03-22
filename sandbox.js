2177c7644f7e870399f6fd3e581bc898da01d8a8
Dmitry <zheliezkov@gmail.com>
2021-03-22 12:48:11 +0200

    new second hw

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,36 @@
-"use strict";
-
-
-const numberOfFilms = +prompt ('How manu movies have you watched?', '');
+'use strict';
+const numberOfFilms = +prompt('How manu films have you watched already?', '');
 
 const personalMovieDB = {
-       count: numberOfFilms,
-       movies: {},
-       actors: {},
-       genres: [],
-       privat: false,
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
-      for ( let i = 0; i < 2; i++) {
-             
-      const a = prompt('one of latest movie?', ''),
-            b = prompt('Can you score it?', '');
+      for (let i = 0; i < 2; i++) {
+          const a = prompt('One of latest movie have you seen?', ''),
+                b = prompt('Can you score it ?', '');
 
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-              personalMovieDB.movies[a] = b;
-              console.log('done');
-            } else {
-                   console.log('errrorr');
-                   i--;
-            }
+                if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+                }else{
+                    console.log('error');
+                    i--;
+                }
 
-         
+            
       }
 
-      if ( personalMovieDB.count < 10) {
-             console.log('You have watched a lot of movies');
-      }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-         console.log('You are a great watcher');
-      }else if ( personalMovieDB >= 30) {
-             console.log('you are good');
-      }else{
-             console.log('there is error');
+      if (personalMovieDB.count < 10) {
+          console.log('you watched not manu films', '');
+      }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('you are a great watcher');
+      } else if ( personalMovieDB.count >= 30){
+          console.log ('you like movies');
+      }else {
+          console.log('this is an error');
       }
       console.log(personalMovieDB);
--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-for (let num = 0; num < 5; num++) {
-    console.log(num);
-}
+"use strict";
+
+const numberOfFilms = +prompt('How manu films have you watched already?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+      for (let i = 0; i < 2; i++) {
+          const a = prompt('One of latest movie have you seen?', ''),
+                b = prompt('Can you score it ?', '');
+
+                if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+                }else{
+                    console.log('error');
+                    i--;
+                }
+
+            
+      }
+
+      if (personalMovieDB.count < 10) {
+          console.log('you watched not manu films', '');
+      }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('you are a great watcher');
+      } else if ( personalMovieDB.count >= 30){
+          console.log ('you like movies');
+      }else {
+          console.log('this is an error');
+      }
+      console.log(personalMovieDB);
