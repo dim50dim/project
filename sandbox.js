ada9de8a3f1ff00bdc2d07ff2e0f442d0a2efce7
dim50dim <zheliezkov@gmail.com>
2021-05-15 22:43:06 +0300

    here i have some trouble

--- sandbox.js
+++ sandbox.js
@@ -6,8 +6,29 @@ function btn () {
   for(let i = 0; i < arr.length; i++){
     if(arr[i] > 0){
       out += arr[i] + ' ';
+      
     }
   }
   document.querySelector('.input').value = out;
 }
 document.querySelector('.btn').onclick = btn;
+
+
+// const  arr = [1, 2, 3, -1, -2, -3];
+
+// function func (arr){
+ 
+//     let newArr = [];
+//     if(newArr.length == 0) {
+//      return alert('that is empty');
+//     }
+    
+//     for(let i = 0; i < arr.length; i++){
+//       if(arr[i] > 0){
+//         return alert(newArr = arr.push(arr[i]));
+      
+//       }
+//     }
+// }
+
+func(arr);
