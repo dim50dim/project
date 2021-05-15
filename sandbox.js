ce8b18db0bc136297d7b3964e663dabe5703f2f2
dim50dim <zheliezkov@gmail.com>
2021-05-15 22:12:46 +0300

    fuck all of this

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,13 @@
-'use strict';
 
-const box = document.getElementById('box');
+const  arr = [1, 2, 3, -1, -2, -3];
 
-console.log(box);
+function btn () {
+  let out = '';
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0){
+      out += arr[i] + ' ';
+    }
+  }
+  document.querySelector('.input').value = out;
+}
+document.querySelector('.btn').onclick = btn;
